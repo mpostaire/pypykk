9c65674afec0e6d2ac49ad8d73399d65c854ff48
mpostaire <maxime_postaire@hotmail.fr>
2022-04-02 22:57:38 +0200

    player idle animation

--- src/actors/player.py
+++ src/actors/player.py
@@ -16,18 +16,30 @@ class Player(arcade.Sprite):
         self.cur_texture = 0
         self.texture = self.player_textures[self.facing_direction][self.cur_texture]
         self.texture_time = 0
+        self.idle_status = 0
 
     def update_animation(self, delta_time):
         self.texture_time += delta_time
 
         if (self.change_x != 0 or self.change_y != 0) and self.texture_time > 0.05:
+            self.idle_status = 0
             self.texture_time = 0
             self.cur_texture = (self.cur_texture + 1) % len(self.player_textures[self.facing_direction])
             self.texture = self.player_textures[self.facing_direction][self.cur_texture]
         elif (self.change_x == 0 and self.change_y == 0):
-            if self.cur_texture != 0:
+            if self.idle_status == 0:
                 self.texture_time = 0
+                self.idle_status = 1
                 self.cur_texture = 0
+            elif self.idle_status == 1 and self.texture_time > 0.3:
+                self.texture_time = 0
+                self.idle_status = 2
+                self.cur_texture = 1
+            elif self.idle_status == 2 and self.texture_time > 0.3:
+                self.texture_time = 0
+                self.idle_status = 1
+                self.cur_texture = 0
+
             self.texture = self.player_textures[self.facing_direction][self.cur_texture]
 
     def on_update(self, delta_time):
